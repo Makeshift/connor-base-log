6179dfdf188ab7131dc567f9cc6bfd57939b8254
Connor Bell <connorbell@fico.com>
2019-10-21 14:11:34 +0100

    Fix: Removed extraneous tags from Sentry

--- index.js
+++ index.js
@@ -65,10 +65,10 @@ class sentryErrorTransport extends Transport {
     log(info, callback) {
         this.emit('logged', info);
         this.sentry.withScope(scope => {
-            [...(config.get("log.sentry.tags") || []), ...(config.get("log.sentry.baseTags") || [])].forEach(tag => {
+            config.get("log.sentry.tags").forEach(tag => {
                 scope.setTag(tag, config.get(tag));
             });
-            [...(config.get("log.sentry.extra") || []), ...(config.get("log.sentry.baseExtra") || [])].forEach(extra => {
+            config.get("log.sentry.extra").forEach(extra => {
                 scope.setExtra(extra, config.get(extra));
             });
             let appConfig = config.getProperties();
